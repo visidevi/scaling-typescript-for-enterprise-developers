b826ba5b7408c63228f287ea67c3f9062c62bbbd
Devi <167757357+visakadev@users.noreply.github.com>
2024-12-30 07:34:33 +0100

    Adding type guards to classes

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
 console.log('Hello, world!');
-var newYork = {
-    name: 'New York',
-    country: 'USA',
-    population: 8000000
-};
-console.log(newYork); // { name: 'New York', country: 'USA', population: 8000000 }
-var london = {
-    name: 'London',
-    country: 'UK',
-};
-console.log(london); // { name: 'London', country: 'UK'
-var tokiyo = {
-    name: 'Tokiyo',
-    country: 'Japan',
-    population: 13929286,
-    capitalBuilding: 'Imperial Palace',
-    landmarks: ['Tokyo Tower', 'Skytree']
-};
-console.log(tokiyo);
-// class  City {
+// interface City {
 //     name: string;
 //     country: string;
-//     constructor(name: string, country: string) {
-//         this.name = name;
-//         this.country = country;
-//     }
-//     getCity():string {
-//         return `${this.name}, ${this.country}`;
-//     }
-//     // Method a function that lives inside the class
-//     private getCityPrivate():string {
-//         return `${this.name}, ${this.country}`;
-//     }
+//     population?: number; 
 // }
-// const berlin = new City('Berlin', 'Germany');
-// console.log(berlin.getCity()); // Berlin, Germany
+// let newYork: City = {
+//     name: 'New York',
+//     country: 'USA',
+//     population: 8000000
+// }
+// console.log(newYork); // { name: 'New York', country: 'USA', population: 8000000 }
+// let london: City = {
+//     name: 'London',
+//     country: 'UK',
+// }
+// console.log(london); // { name: 'London', country: 'UK'
+// interface CapitalCity extends City {
+//     capitalBuilding: string;
+//     landmarks: string[];
+// }
+// const tokio: CapitalCity = {
+//     name: 'tokio',
+//     country: 'Japan',
+//     population: 13929286,
+//     capitalBuilding: 'Imperial Palace',
+//     landmarks: ['Tokyo Tower', 'Skytree']
+// }
+// console.log(tokio);
+var City = /** @class */ (function () {
+    function City(name) {
+        this.name = name;
+    }
+    return City;
+}());
+// Adding type guards to classes
+function outputLocation(location) {
+    if (location instanceof City) {
+        console.log(location.name);
+    }
+    else {
+        console.log(location.toUpperCase());
+    }
+}
+outputLocation(new City('Tokyo')); // Tokyo
+outputLocation('Copenhagen'); // TOKYO
--- script.ts
+++ script.ts
@@ -1,34 +1,49 @@
 console.log('Hello, world!');
-interface City {
-    name: string;
-    country: string;
-    population?: number; 
-}
+// interface City {
+//     name: string;
+//     country: string;
+//     population?: number; 
+// }
 
-let newYork: City = {
-    name: 'New York',
-    country: 'USA',
-    population: 8000000
-}
-console.log(newYork); // { name: 'New York', country: 'USA', population: 8000000 }
-let london: City = {
-    name: 'London',
-    country: 'UK',
-}
-console.log(london); // { name: 'London', country: 'UK'
-interface CapitalCity extends City {
-    capitalBuilding: string;
-    landmarks: string[];
+// let newYork: City = {
+//     name: 'New York',
+//     country: 'USA',
+//     population: 8000000
+// }
+// console.log(newYork); // { name: 'New York', country: 'USA', population: 8000000 }
+// let london: City = {
+//     name: 'London',
+//     country: 'UK',
+// }
+// console.log(london); // { name: 'London', country: 'UK'
+// interface CapitalCity extends City {
+//     capitalBuilding: string;
+//     landmarks: string[];
 
+// }
+
+// const tokio: CapitalCity = {
+//     name: 'tokio',
+//     country: 'Japan',
+//     population: 13929286,
+//     capitalBuilding: 'Imperial Palace',
+//     landmarks: ['Tokyo Tower', 'Skytree']
+// }
+
+// console.log(tokio);
+
+class City {
+   constructor(public name: string) {}
 }
+// Adding type guards to classes
 
-const tokiyo: CapitalCity = {
-    name: 'Tokiyo',
-    country: 'Japan',
-    population: 13929286,
-    capitalBuilding: 'Imperial Palace',
-    landmarks: ['Tokyo Tower', 'Skytree']
+
+function outputLocation(location: City | string) {
+    if (location instanceof City) {
+        console.log(location.name)
+    } else {
+        console.log(location.toUpperCase())
+    }
 }
-
-console.log(tokiyo); 
-
+outputLocation(new City('Tokyo')); 
+outputLocation('Copenhagen'); 
